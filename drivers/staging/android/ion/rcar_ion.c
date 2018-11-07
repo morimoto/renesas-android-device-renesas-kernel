719fb55b78115d2c98271487c9766956407b72ea
Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>
2018-11-07 14:49:04 +0200

    ION: IOCTL: update custom ion ioctl number to fit general ion's rules.
    Change-Id: Ie364da2e599d30c46e52da52c6fbb54f5156b062
    Jira-Id: ADM-2210
    Signed-off-by: Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>

--- drivers/staging/android/ion/rcar_ion.c
+++ drivers/staging/android/ion/rcar_ion.c
@@ -12,16 +12,16 @@ long rcar_ion_get_phys_addr(unsigned long arg)
 {
 	struct dma_buf *dmabuf;
 	struct ion_buffer *buffer = NULL;
-	struct ion_getphys_data data;
+	struct ion_custom_data data;
 	phys_addr_t paddr;
 
 	if (copy_from_user(&data, (void __user *)arg,
-			sizeof(struct ion_getphys_data)))
+			sizeof(struct ion_custom_data)))
 		return -EFAULT;
 
-	dmabuf = dma_buf_get(data.fd);
+	dmabuf = dma_buf_get(data.cmd);
 	if (IS_ERR(dmabuf))
-		return ERR_CAST(dmabuf);
+		return PTR_ERR(dmabuf);
 
 	buffer = (struct ion_buffer *)dmabuf->priv;
 	if (IS_ERR_OR_NULL(buffer) || !buffer->size) {
@@ -32,10 +32,10 @@ long rcar_ion_get_phys_addr(unsigned long arg)
 	paddr = page_to_phys((struct page *)buffer->priv_virt);
 	dma_buf_put(dmabuf);
 
-	data.paddr = (uint64_t)paddr;
+	data.arg = (unsigned long)paddr;
 
 	if (copy_to_user((void __user *)arg, &data,
-				sizeof(struct ion_getphys_data)))
+				sizeof(struct ion_custom_data)))
 		return -EFAULT;
 
 	return 0;
