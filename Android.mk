068b18f6a7c8d682547a8351237aea4a979545c2
Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>
2018-11-30 18:10:24 +0200

    Android fstab: Added 'product' partition entry
     Conflicts:
    	arch/arm64/boot/dts/renesas/Makefile
    
    Jira-ID: ADM-2372
    Change-Id: I4e9ea6b187a5d4c625cf0b8e832889e8fa54d102
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
    Signed-off-by: Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>

--- Android.mk
+++ Android.mk
@@ -68,10 +68,9 @@ endif
 DTBO_BLOBS += $(KERNEL_DTS_DIR)/r8a7795-salvator-4x2g-overlay.dtb --id=0x04779530 \
 	$(KERNEL_DTS_DIR)/r8a7795-salvator-4x2g-overlay.dtb --id=0x04779520 \
 	$(KERNEL_DTS_DIR)/r8a7795v3-salvator-overlay.dtb --id=0x04779530 \
-	$(KERNEL_DTS_DIR)/r8a7795v2-salvator-overlay.dtb --id=0x04779520
-ifeq ($(ENABLE_ADSP),true)
-DTBO_BLOBS += $(KERNEL_DTS_DIR)/salvator-adsp-overlay.dtb --id=0x00779000
-endif
+	$(KERNEL_DTS_DIR)/r8a7795v2-salvator-overlay.dtb --id=0x04779520 \
+	$(KERNEL_DTS_DIR)/salvator-adsp-overlay.dtb --id=0x00779000 \
+	$(KERNEL_DTS_DIR)/partitions-overlay.dtb --id=0x00779000
 endif
 
 ifeq ($(TARGET_PRODUCT),ulcb)
@@ -85,10 +84,9 @@ DTB_BLOBS := $(KERNEL_DTS_DIR)/android-h3-kingfisher.dtb --id=0x0b779520 \
 DTBO_BLOBS := $(KERNEL_DTS_DIR)/r8a7795-h3ulcb-4x2g-overlay.dtb --id=0x0b779530 \
 	$(KERNEL_DTS_DIR)/r8a7795-h3ulcb-4x2g-overlay.dtb --id=0x0b779520 \
 	$(KERNEL_DTS_DIR)/r8a7795v3-h3ulcb-kf-overlay.dtb --id=0x0b779530 \
-	$(KERNEL_DTS_DIR)/r8a7795v2-h3ulcb-kf-overlay.dtb --id=0x0b779520
-ifeq ($(ENABLE_ADSP),true)
-DTBO_BLOBS += $(KERNEL_DTS_DIR)/ulcb-kf-adsp-overlay.dtb --id=0x00779000
-endif
+	$(KERNEL_DTS_DIR)/r8a7795v2-h3ulcb-kf-overlay.dtb --id=0x0b779520 \
+	$(KERNEL_DTS_DIR)/ulcb-kf-adsp-overlay.dtb --id=0x00779000 \
+	$(KERNEL_DTS_DIR)/partitions-overlay.dtb --id=0x00779000
 endif
 
 # Include only for Renesas ones.
