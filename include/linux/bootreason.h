b5f7eabc50f5a7d8894c6764d62f60f2ec6faf15
Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
2019-02-12 17:56:48 +0200

    bootreason: Added ability to set reason
    Change-Id: Ib6c1341e556c72ae980100446309e270338df426
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
    Mantis-ID: (#0004532)
    Jira-ID: ADM-2873

---
+++ include/linux/bootreason.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_BOOTREASON_H_
+#define _LINUX_BOOTREASON_H_
+
+/*
+ * Message written by Linux bootreason driver.
+ * Bootloader must parse it and append to Android bootargs.
+ * Note: sync structure with bootloader and bcb driver.
+ */
+struct bootreason_message {
+	char reason[128];
+	u32 crc;
+};
+
+#ifdef CONFIG_BOOT_REASON
+void shutdown_reason_setup(char *s);
+#endif
+void reboot_reason_setup(char *s);
+
+#endif /* _LINUX_BOOTREASON_H_ */
