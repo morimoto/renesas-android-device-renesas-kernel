85e922edf1a9dcc791bd23d914c65ba2264383ec
Leonid Komaryanskiy <leonid.komaryanskiy@globallogic.com>
2020-05-26 18:18:15 +0300

    added stub for using config fragments
    The option "-T" removed from merge_config.sh because Android "cp" tool
    doesn't support it.
    
    Jira-id: ADM-4813
    Change-Id: Iccf92a6077cac01e0090108beec282da19db1774
    Signed-off-by: Leonid Komaryanskiy <leonid.komaryanskiy@globallogic.com>

--- scripts/kconfig/merge_config.sh
+++ scripts/kconfig/merge_config.sh
@@ -154,7 +154,7 @@ for ORIG_MERGE_FILE in $MERGE_LIST ; do
 done
 
 if [ "$RUNMAKE" = "false" ]; then
-	cp -T -- "$TMP_FILE" "$KCONFIG_CONFIG"
+	cp -- "$TMP_FILE" "$KCONFIG_CONFIG"
 	echo "#"
 	echo "# merged configuration written to $KCONFIG_CONFIG (needs make)"
 	echo "#"
