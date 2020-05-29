3311fcc52ac7e0e054182900d4bf12f5bda8bae4
Vitaliy Vasylskyy <vitaliy.vasylskyy@globallogic.com>
2020-05-29 16:36:35 +0300

    DTS: Android: added Android specific Device Tree files
    For more convenient control by the changes in the Device Tree files, we
    implemented a new hierarchy of android-*.dts[i] files which will contain
    only Android-specific changes in device tree.
    
    Jira-Id: ADM-2242
    Change-Id: I5487e9daeacdbd49f1b87b3c3f771ee94b93860d
    Signed-off-by: Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2016 GloballLogic
+# Copyright (C) 2018 GloballLogic
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,20 +22,21 @@
 # 0x0A Salvator-M
 # 0x0B StarterKit Premier
 
+# Include only for Renesas ones.
+ifneq (,$(filter $(TARGET_PRODUCT), salvator ulcb kingfisher))
 
 # Android makefile to build kernel as a part of Android Build
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 
-LOCAL_PATH := $(call my-dir)
-
-KERNEL_SRC := $(TARGET_KERNEL_SOURCE)
-KERNEL_DEFCONFIG := $(TARGET_KERNEL_CONFIG)
-
-KERNEL_OUT := $(ANDROID_PRODUCT_OUT)/obj/KERNEL_OBJ
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-KERNEL_MODULES_OUT := $(ANDROID_PRODUCT_OUT)/obj/KERNEL_MODULES
-
+KERNEL_DEFCONFIG     := $(TARGET_KERNEL_CONFIG)
+KERNEL_SRC           := $(TARGET_KERNEL_SOURCE)
+KERNEL_OUT           := $(abspath $(PRODUCT_OUT)/obj/KERNEL_OBJ)
+KERNEL_MODULES_OUT   := $(abspath $(PRODUCT_OUT)/obj/KERNEL_MODULES)
+KERNEL_CONFIG        := $(KERNEL_OUT)/.config
+KERNEL_DTS_DIR       := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas
 KERNEL_TARGET_BINARY := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/Image.lz4
+KERNEL_CROSS_COMPILE := $(abspath ./prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-)
+DTB_IMG_OUT          := $(PRODUCT_OUT)/dtb.img
 
 ifeq ($(TARGET_KERNEL_MODULES_OUT),)
 $(warning "TARGET_KERNEL_MODULES_OUT is not set, default path '$(KERNEL_MODULES_OUT)' used")
@@ -45,26 +46,41 @@ ifeq ($(TARGET_USES_HIGHEST_DPI),true)
 DTB_FOOTER := -uhd
 endif
 
-define strip-modules
-    mdpath=`find $(KERNEL_MODULES_OUT) -type f -name modules.order`; \
-    if [ "$$mdpath" != "" ];then \
-        mpath=`dirname $$mdpath`; \
-        ko=`find $$mpath/kernel -type f -name *.ko`; \
-        for i in $$ko; do $(ANDROID_TOOLCHAIN)/aarch64-linux-android-strip --strip-unneeded $$i; \
-        done; \
-    fi
-endef
+ifeq ($(TARGET_PRODUCT),salvator)
+DTB_BLOBS := \
+	$(KERNEL_DTS_DIR)/android-h3-salvator-x.dtb --id=0x00779520 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-x.dtb --id=0x00779610 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-x.dtb --id=0x00779611 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-x.dtb --id=0x00779620 \
+	$(KERNEL_DTS_DIR)/android-h3-salvator-xs.dtb --id=0x04779520 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-xs.dtb --id=0x04779610 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-xs.dtb --id=0x04779611 \
+	$(KERNEL_DTS_DIR)/android-m3-salvator-xs.dtb --id=0x04779620 \
+	$(KERNEL_DTS_DIR)/android-m3n-salvator-xs.dtb --id=0x04779650
+ifeq ($(H3_OPTION),4GB2x2)
+DTB_BLOBS += \
+	$(KERNEL_DTS_DIR)/r8a7795-salvator-xs-2x2g.dtb --id=0x04779530
+else
+DTB_BLOBS += \
+	$(KERNEL_DTS_DIR)/r8a7795-salvator-xs.dtb --id=0x04779530
+endif
+endif
 
-ifeq ($(TARGET_ARCH),arm64)
-    ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
-        ifeq ($(HOST_OS),darwin)
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/aarch64-linux-android-"
-        else
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/aarch64-linux-android-"
-        endif
-    else
-        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_TOOLCHAIN)/aarch64-linux-android-"
-    endif
+ifeq ($(TARGET_PRODUCT),ulcb)
+DTB_BLOBS := \
+    $(KERNEL_DTS_DIR)/android-h3ulcb.dtb --id=0x0b779520
+endif
+
+ifeq ($(TARGET_PRODUCT),kingfisher)
+DTB_BLOBS := $(KERNEL_DTS_DIR)/android-h3-kingfisher.dtb --id=0x0b779520 \
+	$(KERNEL_DTS_DIR)/android-h3-kingfisher.dtb --id=0x0b779530
+endif
+
+# Include only for Renesas ones.
+ifeq ($(DTB_BLOBS),)
+ifneq (,$(filter $(TARGET_PRODUCT), salvator ulcb kingfisher))
+$(error "DTB_BLOBS is not set for target product $(TARGET_PRODUCT)")
+endif
 endif
 
 ifeq ($(TARGET_KERNEL_EXT_MODULES),)
@@ -78,37 +94,27 @@ $(KERNEL_MODULES_OUT):
 	mkdir -p $(KERNEL_MODULES_OUT)
 
 $(KERNEL_CONFIG): $(KERNEL_OUT)
-	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) VARIANT_DEFCONFIG=$(VARIANT_DEFCONFIG) SELINUX_DEFCONFIG=$(SELINUX_DEFCONFIG) $(KCFLAGS) $(KERNEL_DEFCONFIG)
+	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(KERNEL_DEFCONFIG)
 
-$(KERNEL_TARGET_BINARY): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_MODULES_OUT)
-	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) $(KCFLAGS) Image.lz4 dtbs
+$(KERNEL_TARGET_BINARY): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_MODULES_OUT) $(DTC)
+	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) $(KCFLAGS) Image.lz4 dtbs
 
 TARGET_KERNEL_MODULES: $(KERNEL_TARGET_BINARY)
-	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) INSTALL_MOD_PATH=$(KERNEL_MODULES_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) modules
-	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) INSTALL_MOD_PATH=$(KERNEL_MODULES_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) modules_install
+	# clearing old kernel modules:
+	find $(KERNEL_MODULES_OUT) -maxdepth 1 -type f -name '*.ko' | xargs -I{} rm -f {}
+	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) INSTALL_MOD_PATH=$(KERNEL_MODULES_OUT) ARCH=$(TARGET_ARCH) $(KCFLAGS) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) modules
+	$(MAKE) -C $(KERNEL_SRC) O=$(KERNEL_OUT) INSTALL_MOD_PATH=$(KERNEL_MODULES_OUT) ARCH=$(TARGET_ARCH) $(KCFLAGS) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) modules_install
+	# copying kernel modules with correct vermagic:
+	find $(KERNEL_MODULES_OUT) -mindepth 2 -type f -name '*.ko' | grep "$(shell head -1 $(KERNEL_OUT)/include/config/kernel.release)" | xargs -I{} cp {} $(KERNEL_MODULES_OUT)
 
 $(TARGET_KERNEL_EXT_MODULES) : TARGET_KERNEL_MODULES
 
-# Below workaround for kernel modules dependencies in modules.mk:
-# > $(BOARD_VENDOR_KERNEL_MODULES) : $(ANDROID_PRODUCT_OUT)/kernel
-# In original code the path was hardcoded. But PRODUCT_OUT variable not
-# available in modules.mk, so we must make fake target to resolve that deps.
-ifneq ($(PRODUCT_OUT),$(ANDROID_PRODUCT_OUT))
-$(ANDROID_PRODUCT_OUT)/kernel: $(PRODUCT_OUT)/kernel
-endif
 
-$(PRODUCT_OUT)/kernel: $(TARGET_KERNEL_EXT_MODULES) mkdtimg
+$(DTB_IMG_OUT): $(TARGET_KERNEL_EXT_MODULES) mkdtimg
+	mkdtimg create $(PRODUCT_OUT)/dtb.img --page_size=4096 $(DTB_BLOBS)
+
+$(PRODUCT_OUT)/kernel: $(BOARD_PREBUILT_DTBOIMAGE) $(DTB_IMG_OUT)
 	cp -v $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/Image.lz4 $(PRODUCT_OUT)/kernel
-	mkdtimg create $(PRODUCT_OUT)/dtb.img --page_size=4096 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7795-es1-salvator-x$(DTB_FOOTER).dtb --id=0x00779510 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7795-es1-salvator-x$(DTB_FOOTER).dtb --id=0x00779511 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7795-salvator-x.dtb --id=0x00779520 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-x.dtb --id=0x00779610 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-x.dtb --id=0x00779611 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-x.dtb --id=0x00779620 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7795-salvator-xs.dtb --id=0x04779520 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-xs.dtb --id=0x04779610 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-xs.dtb --id=0x04779611 \
-		$(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/renesas/r8a7796-salvator-xs.dtb --id=0x04779620
 
 endif # TARGET_PREBUILT_KERNEL
+endif # TARGET_PRODUCT salvator ulcb kingfisher
