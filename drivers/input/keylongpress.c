bb227d710f45efda9521bccd39f3cd1b21807cb9
Mykyta Poturai <mykyta.poturai@globallogic.com>
2020-08-18 10:52:14 +0300

    keylongpress: Rewrite keylongpress driver
    Keylongpress driver can now work without keycombo and keyreset drivers.
    Multiple long press actions can be configured in a device-tree.
    
    Jira-Id: ADM-5020
    Signed-off-by: Mykyta Poturai <mykyta.poturai@globallogic.com>
    Change-Id: I772abfbad4d2f25117567a1eafc4ccd4190cfe84

--- drivers/input/keylongpress.c
+++ drivers/input/keylongpress.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 GlobalLogic
+ * Copyright (C) 2018 - 2020 GlobalLogic
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,62 +18,259 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/input.h>
-#include <linux/keyreset.h>
 #include <linux/reboot.h>
 #include <linux/of.h>
+#include <linux/slab.h>
 #include <linux/bootreason.h>
 
-#define DEFAULT_KEY_DOWN_DELAY 10000
+#define DEFAULT_KEY_DOWN_DELAY (2*HZ)
+#define KEYLONGPRESS_NAME "keylongpress"
 
-int keylongpress_fn(void)
+struct keylongpress_task {
+	unsigned long keybits[BITS_TO_LONGS(KEY_CNT)];
+	unsigned int delay;
+	int target_pressed;
+	int currently_pressed;
+	const char *action;
+	struct delayed_work wait_key_down;
+
+};
+
+struct keylongpress_data {
+	struct input_handler handler;
+	spinlock_t lock;
+	int tasks_count;
+	struct keylongpress_task *active;
+	struct keylongpress_task tasks[];
+};
+
+
+static void do_action(struct work_struct *work)
 {
+	struct delayed_work *dwork =
+		container_of(work, struct delayed_work, work);
+	struct keylongpress_task *task =
+		container_of(dwork, struct keylongpress_task, wait_key_down);
+
+	if (!strcmp("reboot", task->action)) {
+		kernel_restart("android");
+	} else if (!strcmp("recovery", task->action)) {
+		kernel_restart("recovery");
+	} else if (!strcmp("fastboot", task->action)) {
 #ifdef CONFIG_BOOT_REASON
-	shutdown_reason_setup("powerkey");
+		recovery_command_setup("recovery\n--fastboot\n");
 #endif
-	orderly_poweroff(true);
-	return 0;
+		kernel_restart("recovery");
+	} else {
+		pr_err("Keylongpress unknown acton %s\n", task->action);
+	}
 }
 
-static struct keyreset_platform_data keylongpress_pdata = {
-	.keys_down = {
-		KEY_POWER,
-		0
-	},
-	.reset_fn = keylongpress_fn,
-	.key_down_delay = DEFAULT_KEY_DOWN_DELAY,
-};
+static void stop_task(struct keylongpress_data *priv, struct keylongpress_task *task)
+{
+	if (task != priv->active)
+		return;
 
-static struct platform_device keylongpress_device = {
-	.name = KEYRESET_NAME,
-	.dev.platform_data = &keylongpress_pdata,
-};
+	cancel_delayed_work(&task->wait_key_down);
+	priv->active = NULL;
+}
+
+static void start_task(struct keylongpress_data *priv, struct keylongpress_task *task)
+{
+	if (priv->active == task)
+		return;
+
+	if (!priv->active) {
+		schedule_delayed_work(&task->wait_key_down, task->delay);
+		priv->active = task;
+		return;
+	}
+
+	if (task->target_pressed > priv->active->target_pressed) {
+		stop_task(priv, priv->active);
+		schedule_delayed_work(&task->wait_key_down, task->delay);
+		priv->active = task;
+	}
+}
+
+static void keylongpress_event(struct input_handle *handle, unsigned int type,
+		unsigned int code, int value)
+{
+	struct keylongpress_task *task;
+	struct keylongpress_data *priv = handle->private;
+	int i;
+
+	if (type != EV_KEY)
+		return;
+
+	spin_lock(&priv->lock);
+	for (i = 0; i < priv->tasks_count; i++) {
+		task = &priv->tasks[i];
+
+		if (test_bit(code, task->keybits)) {
+			if (value)
+				task->currently_pressed++;
+			else
+				task->currently_pressed--;
+		}
+		if (task->currently_pressed == task->target_pressed)
+			start_task(priv, task);
+		else
+			stop_task(priv, task);
+	}
+	spin_unlock(&priv->lock);
+}
+
+static int keylongpress_connect(struct input_handler *handler,
+		struct input_dev *dev,
+		const struct input_device_id *id)
+{
+	int ret;
+	struct input_handle *handle;
+	struct keylongpress_data *priv = container_of(handler,
+			struct keylongpress_data, handler);
+
+	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = KEYLONGPRESS_NAME;
+	handle->private = priv;
+
+	ret = input_register_handle(handle);
+	if (ret)
+		goto err;
+	ret = input_open_device(handle);
+	if (ret)
+		goto err;
+
+	return 0;
+err:
+	pr_err("keylongpress: Unable to connect input device: %d\n", ret);
+	kfree(handle);
+	return ret;
+}
+
+static void keylongpress_disconnect(struct input_handle *handle)
+{
+	input_unregister_handle(handle);
+	input_close_device(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id device_ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) }
+	},
+	{ }
+}
+MODULE_DEVICE_TABLE(input, device_ids);
 
 static int keylongpress_probe(struct platform_device *pdev)
 {
+	struct keylongpress_data *priv;
 	struct device *dev = &pdev->dev;
+	struct device_node *child;
+	int num_children, ret;
+	int key, keycount;
+	int count = 0;
+	int i;
 
-	device_property_read_u32(dev, "key_down_delay",
-			&keylongpress_pdata.key_down_delay);
+	num_children = of_get_available_child_count(dev->of_node);
+	priv = devm_kzalloc(dev, sizeof(priv) +
+			sizeof(priv->tasks[0]) * num_children, GFP_KERNEL);
+	if (!priv) {
+		pr_err("Failed to allocate memory for keylongpress_data\n");
+		return -ENOMEM;
+	}
 
-	platform_device_register(&keylongpress_device);
+	priv->tasks_count = num_children;
 
-	pr_info("keylongpress: button code %d, delay %d ms\n",
-			keylongpress_pdata.keys_down[0], keylongpress_pdata.key_down_delay);
+	for_each_child_of_node(dev->of_node, child) {
+		keycount = of_property_count_u32_elems(child, "keys");
+
+		if (keycount < 1) {
+			dev_err(dev, "Invalid keylongpress definiton %s: can't read keys\n", child->name);
+			continue;
+		}
+		priv->tasks[count].target_pressed = keycount;
+
+		for (i = 0; i < keycount; i++) {
+			ret = of_property_read_u32_index(child, "keys", i,
+					&key);
+			if (ret) {
+				dev_err(dev, "Invalid keylongpress definiton %s: can't read keys\n", child->name);
+				continue;
+			}
+			__set_bit(key, priv->tasks[count].keybits);
+		}
+
+		ret = of_property_read_u32(child, "timeout-ms",
+				&priv->tasks[count].delay);
+		if (ret)
+			priv->tasks[count].delay = DEFAULT_KEY_DOWN_DELAY;
+		else
+			priv->tasks[count].delay = msecs_to_jiffies(priv->tasks[count].delay);
+
+		ret = of_property_read_string(child, "action",
+				&priv->tasks[count].action);
+		if (ret) {
+			dev_err(dev, "Invalid keylongpress definiton %s: can't read action\n", child->name);
+			continue;
+		}
+
+		INIT_DELAYED_WORK(&priv->tasks[count].wait_key_down, do_action);
+		count++;
+	}
+	priv->tasks_count = count;
+	spin_lock_init(&priv->lock);
+
+	priv->handler.name = KEYLONGPRESS_NAME;
+	priv->handler.id_table = device_ids;
+	priv->handler.event = keylongpress_event;
+	priv->handler.connect = keylongpress_connect;
+	priv->handler.disconnect = keylongpress_disconnect;
+
+	ret = input_register_handler(&priv->handler);
+	if (ret) {
+		dev_err(dev, "Unable to register input handler\n");
+		goto err;
+	}
+
+	platform_set_drvdata(pdev, priv);
+	return 0;
+err:
+	return ret;
+}
+
+static int keylongpress_remove(struct platform_device *pdev)
+{
+	struct keylongpress_data *priv = platform_get_drvdata(pdev);
+	int i;
+
+	input_unregister_handler(&priv->handler);
+	for (i = 0; i < priv->tasks_count; i++)
+		cancel_delayed_work_sync(&priv->tasks[i].wait_key_down);
 	return 0;
 }
 
 static const struct of_device_id keylongpress_of_match[] = {
-	{ .compatible = "key-long-press", },
+	{
+		.compatible = KEYLONGPRESS_NAME,
+	},
 	{ },
 };
-MODULE_DEVICE_TABLE(of, keylongpress_of_match);
 
 static struct platform_driver keylongpress_driver = {
-	.probe		= keylongpress_probe,
-	.driver		= {
-		.name   = "keylongpress",
+	.driver = {
+		.name = KEYLONGPRESS_NAME,
 		.of_match_table = keylongpress_of_match,
-	}
+	},
+	.probe = keylongpress_probe,
+	.remove = keylongpress_remove
 };
 
 static int __init keylongpress_init(void)
@@ -83,14 +280,13 @@ static int __init keylongpress_init(void)
 
 static void __exit keylongpress_exit(void)
 {
-	platform_driver_unregister(&keylongpress_driver);
-	platform_device_unregister(&keylongpress_device);
+	return platform_driver_unregister(&keylongpress_driver);
 }
 
 module_init(keylongpress_init);
 module_exit(keylongpress_exit);
 
-MODULE_AUTHOR("Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>");
+MODULE_AUTHOR("Mykyta Poturai <mykyta.poturai@globallogic.com>");
 MODULE_DESCRIPTION("Long press button driver");
 MODULE_VERSION("0.1");
 MODULE_LICENSE("GPL");
